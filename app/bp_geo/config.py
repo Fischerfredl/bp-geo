61b90d651aa4eaa90944ff1db6b8a57868327763
amelch <alfred.melch@gmx.de>
2017-09-17 14:32:22 +0200

    added docker secrets support

--- app/bp_geo/config.py
+++ app/bp_geo/config.py
@@ -1,12 +1,20 @@
 import os
 
+def get_secret(secret_name):
+    try:
+        with open('run/secrets/{0}'.format(secret_name), 'r') as secret_file:
+            return secret_file.read()
+    except IOError:
+        return None
+
 
 def init_config(app):
-    app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'initial_key')
+    app.config['SECRET_KEY'] = get_secret('SECRET_KEY') or 'initial_key'
 
     app.config['MONGODB_DB'] = os.getenv('MONGODB_DB', 'development')
     app.config['MONGODB_HOST'] = os.getenv('MONGODB_HOST', 'mongomock://localhost')
     app.config['MONGODB_PORT'] = os.getenv('MONGODB_PORT', 27017)
-    app.config['MONGODB_USERNAME'] = os.getenv('MONGODB_USERNAME')
-    app.config['MONGODB_PASSWORD'] = os.getenv('MONGODB_PASSWORD')
+
+    app.config['MONGODB_USERNAME'] = get_secret('mongodb_username')
+    app.config['MONGODB_PASSWORD'] = get_secret('mongodb_password')
     return
